14ff1834d2bb1f0647bd15db5a594b0cad8bfb9d
Anna-LM <annallambiasmaw@gmail.com>
2024-09-16 17:27:57 +0100

    flask error handling

--- Code/Flask_Application.py
+++ Code/Flask_Application.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
@@ -13,5 +14,18 @@ def return_weather():
 def return_history():
     return "Most Recent searches ... " , 200
 
+#Error Handling with Flask Endpoints
+#ToDo: add more detailed error messages
+@app.errorhandler(HTTPException)
+def handle_exception(e): 
+    response = e.get_response()
+    if e.code == 405:
+        response = 'Please use Get'
+    elif e.code == 404:
+        response = 'Please use weather or history'
+    else:
+        response = 'Unprecidented error, please try again'
+
+    return response
 
 app.run()
